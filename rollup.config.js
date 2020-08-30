2f34db8d55f197bbf45d122f9c8720c8a87202ab
Cihad Paksoy <cihadp@gmail.com>
2020-08-30 13:22:39 +0300

    terser

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,37 @@
 import svg from 'rollup-plugin-vue-inline-svg'
 import vue from 'rollup-plugin-vue'
+import { terser } from "rollup-plugin-terser";
 
 export default {
 	input: 'src/main.js',
 	external: ["vue", '@contentarchitect/editor'],
-	output: {
-		file: 'dist/divider.js',
-		format: 'umd',
-		name: 'Divider',
-		globals: {
-			vue: "Vue",
-			'@contentarchitect/editor': 'ContentArchitect'
-		}
-	},
+	output: [
+		{
+			file: 'dist/divider.js',
+			format: 'umd',
+			name: 'Divider',
+			globals: {
+				vue: "Vue",
+				'@contentarchitect/editor': 'ContentArchitect'
+			}
+		},
+		{
+			file: 'dist/divider.min.js',
+			format: 'umd',
+			name: 'Divider',
+			globals: {
+				vue: "Vue",
+				'@contentarchitect/editor': 'ContentArchitect'
+			},
+			plugins: [
+				terser({
+					keep_classnames: true
+				}),
+			]
+		},
+	],
 	plugins: [
 		svg(),
-		vue()
-	]
+		vue(),
+	],
 }
